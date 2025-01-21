48b46bf0da7e304f5ce01de52fbdb9e7053cc3ec
Mehul Uttam <mehulsavio@gmail.com>
2025-01-21 13:40:12 +0530

    Add TextAnimate component and enhance problem statements page with animations

--- src/app/(layout)/problem-statements/page.jsx
+++ src/app/(layout)/problem-statements/page.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Card } from "@/components/Card";
 import { cardData } from "@/lib/data/cardData";
+import { TextAnimate } from "@/components/ui/text-animate";
 
 const Page = () => {
   return (
     <div className="bg-blue-700">
-      <div className="w-full flex items-center justify-center p-10">
-        <h1 className="text-balance text-5xl font-semibold leading-none tracking-tighter sm:text-6xl md:text-7xl lg:text-8xl">
+      <div className="w-full flex items-center justify-center p-20">
+        <h1 className="text-balance text-5xl font-semibold leading-none tracking-tighter sm:text-2xl md:text-4xl lg:text-7xl capitalize text-white">
+          <TextAnimate animation="blurInUp" by="character" className={"font-mono"}>
+            Problem Statements
+          </TextAnimate>
         </h1>
       </div>
       <div className=" min-h-screen bg-indigo-800 p-8 rounded-t-3xl">
