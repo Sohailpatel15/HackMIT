f74ac68b1f910d179dadb2e5a57c39bc1ce1ccef
Atharva Arbat <95687752+atharvaarbat@users.noreply.github.com>
2025-02-02 17:26:05 +0530

    Update Spline.jsx

--- src/app/(layout)/home2/_components/Spline.jsx
+++ src/app/(layout)/home2/_components/Spline.jsx
@@ -4,7 +4,7 @@
 export default function SplineView({children}) {
   return (
     <main className='relative h-screen w-screen overflow-hidden'>
-      <iframe src='https://my.spline.design/backgroundhoverinteractioncopy-3ec32343152014d7136a5dc2a03a1f20/' width='100%' height='100%'></iframe>
+      <iframe src='https://my.spline.design/noisyglasscube-ab7eba01349a6bc6123a4bb7d0f0f841/' width='100%' height='100%'></iframe>
       <div className='absolute inset-0 z-10 pointer-events-none'>
         {children}
       </div>
