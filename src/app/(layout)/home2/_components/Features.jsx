d3c76e35bef252cfb31d1121ab89f8e7ee7a5caf
Sohail Patel <patelsohailp7@gmail.com>
2025-02-11 00:46:38 +0530

    error fixed in features section

--- src/app/(layout)/home2/_components/Features.jsx
+++ src/app/(layout)/home2/_components/Features.jsx
@@ -20,7 +20,7 @@ const BentoTilt = ({ children, className = "" }) => {
     const tiltX = (relativeY - 0.5) * 10;
     const tiltY = (relativeX - 0.5) * -10;
 
-    const newTransform = `perspective(700px) rotateX(${tiltX}deg) rotateY(${tiltY}deg) scale3d(0.98, 0.98, 0.98)`;
+    const newTransform = ``;
 
     settransformStyle(newTransform);
   };
@@ -51,7 +51,7 @@ const BentoCard = ({ src, title, description }) => {
         autoPlay
         className="absolute left-0 top-0 size-full object-cover object-center"
       />
-      <div className="relative z-10 flex size-full flex-col justify-between p-5 text-blue-50">
+      <div className="relative z-10 flex size-full flex-col justify-between p-5 text-hack-300">
         <div>
           <h1 className="bento-title special-font">{title}</h1>
           {description && (
@@ -83,7 +83,7 @@ const Features = () => {
                 Inn<b>o</b>vate
               </>
             }
-            description="Transform your ideas into reality at HACKMIT-WPU, where developers, designers, and visionaries collaborate to create groundbreaking projects."
+            description="Transform your ideas into reality at HACKMIT-WPU."
           />
         </BentoTilt>
         <div className="grid h-[135vh] grid-cols-2 grid-rows-3 gap-7">
@@ -95,7 +95,7 @@ const Features = () => {
                   Coll<b>a</b>borate
                 </>
               }
-              description="Team up with like-minded individuals, brainstorm solutions, and build something extraordinary in just 36 hours."
+              description=""
             />
           </BentoTilt>
           <BentoTilt className="bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0">
@@ -106,7 +106,7 @@ const Features = () => {
                   Cod<b>e</b>
                 </>
               }
-              description="Write, debug, and deploy your ideas with the latest tools and technologies, competing for the top spot at HACKMIT-WPU."
+              description=""
             />
           </BentoTilt>
           <BentoTilt className="bento-tilt_1 me-14 md:col-span-1 md:me-0">
@@ -117,7 +117,7 @@ const Features = () => {
                   S<b>h</b>owcase
                 </>
               }
-              description="Present your project to industry experts and gain recognition for your innovative solutions at the grand finale."
+              description=""
             />
           </BentoTilt>
 
